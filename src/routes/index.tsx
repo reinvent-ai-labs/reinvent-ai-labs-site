0d8cec496f702a1005d4b8e9e58bdcbab7a1dd5f
Mahidhar Vuppu <159865324+MahidharVuppu@users.noreply.github.com>
2026-07-25 13:34:35 -0400

    Update index.tsx
    added links

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -34,14 +34,34 @@ export const Route = createFileRoute("/")({
     ],
 
     links: [
-      {
-        rel: "icon",
-        type: "image/png",
-        sizes: "96x96",
-        href: "/favicon-96x96.png",
-      },
-    ],
-  }),
+  {
+    rel: "icon",
+    type: "image/png",
+    sizes: "48x48",
+    href: "/favicon-48x48.png",
+  },
+  {
+    rel: "icon",
+    type: "image/png",
+    sizes: "32x32",
+    href: "/favicon-32x32.png",
+  },
+  {
+    rel: "icon",
+    type: "image/png",
+    sizes: "16x16",
+    href: "/favicon-16x16.png",
+  },
+  {
+    rel: "shortcut icon",
+    href: "/favicon.ico",
+  },
+  {
+    rel: "apple-touch-icon",
+    sizes: "180x180",
+    href: "/apple-touch-icon.png",
+  },
+],
 
   component: Home,
 });
